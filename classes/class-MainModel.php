4919ecb517054719e604080174a732f22d95d124
PIFER\paulo.ramos <paulovitor.ramos@pifer.com.br>
2017-08-14 12:39:33 -0300

    Commit

--- classes/class-MainModel.php
+++ classes/class-MainModel.php
@@ -1,8 +1,39 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * MainModel - Modelo geral
+ *
+ * 
+ *
+ * @package TutsupMVC
+ * @since 0.1
  */
-
+class MainModel
+{
+    public $form_data;
+    public $form_msg;
+    public $form_confirma;
+    public $db;
+    public $controller;
+    public $parametros;
+    public $userdata;
+    public function inverte_data( $data = null ) {
+        $nova_data = null;
+        if ( $data ) {
+            $data = preg_split('/-|/|s|:/', $data);
+            $data = array_map( 'trim', $data );
+            $nova_data .= chk_array( $data, 2 ) . '-';
+            $nova_data .= chk_array( $data, 1 ) . '-';
+            $nova_data .= chk_array( $data, 0 );
+            if ( chk_array( $data, 3 ) ) {
+                $nova_data .= ' ' . chk_array( $data, 3 );
+            }
+            if ( chk_array( $data, 4 ) ) {
+                $nova_data .= ':' . chk_array( $data, 4 );
+            }
+            if ( chk_array( $data, 5 ) ) {
+                $nova_data .= ':' . chk_array( $data, 5 );
+            }
+        }
+        return $nova_data;
+    } // inverte_data
+} // MainModel
